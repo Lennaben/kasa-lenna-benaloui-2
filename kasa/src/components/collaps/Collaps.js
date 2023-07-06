9720cc03d4aa18fde05215f9b2e524154bbebd36
Lennaben <BENALOUILENNA@GMAIL.COM>
2023-07-06 17:30:45 +0200

    arrow collaps

--- kasa/src/components/collaps/Collaps.js
+++ kasa/src/components/collaps/Collaps.js
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import "./Collaps.css"
+import ImgArrow from "../../assets/arrow.svg"
 
 const Collaps = (props) => {
   const [open, setOPen] = useState(false)
@@ -10,16 +11,15 @@ const Collaps = (props) => {
 
   return (
     <div className={`container-collaps ${props.about && "large-width"}`}>
-    
       <button className="btn-collaps" onClick={toggle}>
         {props.label}
+
+        <img className={open && "arrow_close"} src={ImgArrow} alt="" />
       </button>
 
       {open && <div className="toggle">{props.children}</div>}
     </div>
   )
 }
-
-
 
 export default Collaps
